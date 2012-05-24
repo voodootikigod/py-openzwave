ee3f8b0260ad83b6dad1e5600df1eed36c09179b
Chris Williams <chris@saferaging.com>
2012-05-24 10:59:57 -0400

    Adding darwin as a compile target to handle Mac OS X builds.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from os import name as os_name
+from sys import platform as os_platform
 from distutils.core import setup
 from Cython.Distutils import extension
 from Cython.Distutils import build_ext
@@ -10,6 +11,13 @@ if os_name == 'nt':
                              extra_objects=['openzwave/cpp/lib/windows-mingw32/openzwave.a'],
                              include_dirs=['openzwave/cpp/src', 'openzwave/cpp/src/value_classes', 'openzwave/cpp/src/platform']
     )]    
+elif os_platform == 'darwin':
+    ext_modules = [extension.Extension("openzwave", ["openzwave.pyx"],
+                             libraries=['udev', 'stdc++'],
+                             language="c++",
+                             extra_objects=['openzwave/cpp/lib/mac/openzwave.a'], 
+                             include_dirs=['openzwave/cpp/src', 'openzwave/cpp/src/value_classes', 'openzwave/cpp/src/platform']
+    )]
 else:
     ext_modules = [extension.Extension("openzwave", ["openzwave.pyx"],
                              libraries=['udev', 'stdc++'],
